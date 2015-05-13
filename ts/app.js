95f3c774c08b22cb483cb3c02923e04a21ffebd9
Hitoshi Honma <honma_hitoshi@sirok.co.jp>
2015-05-13 19:41:59 +0900

    Remove JS file

--- ts/app.js
+++
@@ -1,9 +0,0 @@
-var GrowthMessage;
-(function (GrowthMessage) {
-    var App = (function () {
-        function App() {
-        }
-        return App;
-    })();
-    GrowthMessage.App = App;
-})(GrowthMessage || (GrowthMessage = {}));
