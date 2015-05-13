8a8783cb70d84fdc1f6d69e7075c1b077e0ce718
Hitoshi Honma <honma_hitoshi@sirok.co.jp>
2015-05-13 17:36:02 +0900

    Add grunt

---
+++ Gruntfile.js
@@ -0,0 +1,11 @@
+module.exports = function(grunt){
+
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+    });
+
+    require('load-grunt-tasks')(grunt);
+
+    grunt.registerTask('default', ['']);
+
+};
