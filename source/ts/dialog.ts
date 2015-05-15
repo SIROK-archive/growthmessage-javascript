0f93c779ce2015f852b55337dc388dcba21a0060
Hitoshi Honma <honma_hitoshi@sirok.co.jp>
2015-05-15 18:51:21 +0900

    Add functions that fit dialog and overlay

--- source/ts/dialog.ts
+++ source/ts/dialog.ts
@@ -3,6 +3,7 @@
 
 module GrowthMessage {
     export class Dialog extends GrowthMessage.Events {
+        private el:any;
         private parentElement:any;
         private templates = {
             'plain' : 'dialog-text.html',
@@ -14,6 +15,10 @@ module GrowthMessage {
         open(data:{type:string}) {
             this.parentElement = document.body.getElementsByClassName('growthmessage')[0];
             this.render(data);
+            this.setElement();
+            this.fitOverlay();
+            this.fitDialog();
+            this.scrollToTop();
         }
         render(data:{type:string}) {
             var template = GrowthMessage.module.require(this.templates[data.type]);
@@ -33,6 +38,35 @@ module GrowthMessage {
             });
             data.buttons = newButtons;
             return data;
+        }
+        setElement() {
+            this.el = document.body.getElementsByClassName('growthmessage-dialog')[0];
+        }
+        fitOverlay() {
+            var D = document;
+            this.el.style.width = Math.max(
+                D.body.scrollWidth, D.documentElement.scrollWidth,
+                D.body.offsetWidth, D.documentElement.offsetWidth,
+                D.body.clientWidth, D.documentElement.clientWidth
+            ) + 'px';
+            this.el.style.height = Math.max(
+                D.body.scrollHeight, D.documentElement.scrollHeight,
+                D.body.offsetHeight, D.documentElement.offsetHeight,
+                D.body.clientHeight, D.documentElement.clientHeight
+            ) + 'px';
+        }
+        fitDialog() {
+            var D = document;
+            var el:any = document.body.getElementsByClassName('growthmessage-dialog__inner')[0];
+            el.style.width = Math.max(
+                D.body.clientWidth, D.documentElement.clientWidth
+            ) + 'px';
+            el.style.height = Math.min(
+                D.body.clientHeight, D.documentElement.clientHeight
+            ) + 'px';
+        }
+        scrollToTop() {
+            window.scrollTo(0, 1);
         }
     }
 }
